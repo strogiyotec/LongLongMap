a7c141c5339035482fb3fae0b602906c4fe693d6
strogiyotec <almas337519@gmail.com>
2022-08-14 21:30:05 -0700

    Unsafe usage
    Unsafe was injected into LongLongMap
    next we need to come up with hashing algorithm
    The open addressing must be used

--- src/main/java/com/almas/main/HashLongLongMap.java
+++ src/main/java/com/almas/main/HashLongLongMap.java
@@ -1,13 +1,52 @@
 package com.almas.main;
 
-public final class HashLongLongMap implements LongLongMap{
+import sun.misc.Unsafe;
+
+/**
+ * This class allocates a contiguous block of memory and stores two longs there
+ * <p>
+ * --------------|----------|------------|-------
+ * key           |  value   |  key       | value
+ * --------------|----------|------------|-------
+ * <p>
+ * The entry size is two longs
+ * TODO: create hashing algorithm for open addressing
+ */
+public final class HashLongLongMap implements LongLongMap {
+
+    /**
+     * One entry can contain 2 longs.
+     */
+    private static final long ENTRY_SIZE = (long) (Long.BYTES << 1);
+
+    private final Unsafe unsafe;
+
+    /**
+     *
+     */
+    private final long startAddress;
+
+    /**
+     * How many entries this map can handle
+     */
+    private final long maxEntries;
+
+    public HashLongLongMap(final Unsafe unsafe, final long startAddress, final long capacity) {
+        this.unsafe = unsafe;
+        this.startAddress = startAddress;
+        this.maxEntries = capacity / HashLongLongMap.ENTRY_SIZE;
+        //clean up the memory
+        this.unsafe.setMemory(startAddress, capacity, (byte) 0);
+
+    }
+
     @Override
     public long put(final long key, final long value) {
-        throw new UnsupportedOperationException("#put()");
+        throw new UnsupportedOperationException("TODO implement");
     }
 
     @Override
     public long get(final long key) {
-        throw new UnsupportedOperationException("#get()");
+        throw new UnsupportedOperationException("TODO implement");
     }
 }
--- src/test/java/com/almas/main/HashLongLongTestCase.java
+++ src/test/java/com/almas/main/HashLongLongTestCase.java
@@ -1,17 +1,34 @@
 package com.almas.main;
 
+import java.lang.reflect.Field;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import sun.misc.Unsafe;
 
 /**
  * {@link HashLongLongMap} test cases.
  */
 public class HashLongLongTestCase {
 
+    private static Unsafe unsafe;
+
+    @BeforeAll
+    public static void init() throws Exception {
+        HashLongLongTestCase.unsafe = HashLongLongTestCase.fetchInstance();
+    }
+
     @Test
     @Disabled("TODO::")
     public void testPut() {
-        final HashLongLongMap hashLongLongMap = new HashLongLongMap();
-        hashLongLongMap.put(0L,0L);
+        final HashLongLongMap hashLongLongMap = new HashLongLongMap(unsafe, 0, 0);
+        hashLongLongMap.put(0L, 0L);
+    }
+
+    private static Unsafe fetchInstance() throws Exception {
+        Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
+        theUnsafe.setAccessible(true);
+        unsafe = (Unsafe) theUnsafe.get(null);
+        return unsafe;
     }
 }
